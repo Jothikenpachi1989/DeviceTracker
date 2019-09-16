f2ec25f6584012ec19279d2c7d378e3cc37f9eb9
Lawz <lawzuns@gmail.com>
2019-09-16 23:42:02 +0530

    Revert "Update with native base"
    This reverts commit 000d285c4425fcde47a24b23adbcbd86aebd224b.

--- Screens/Admin/AdminPage.js
+++ Screens/Admin/AdminPage.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Container, Header, Title, Button, Icon} from 'native-base';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import { Card, Icon } from 'react-native-elements';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -9,38 +10,48 @@ const instructions = Platform.select({
 });
 
 export default class AdminPage extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+    headerTintColor: '#ffffff',
+      headerStyle: {
+        backgroundColor: '#2F95D6',
+        borderBottomColor: '#ffffff',
+        borderBottomWidth: 3,
+      },
+      headerTitleStyle: {
+        fontSize: 18,
+      },headerRight: (
+        <Button
+          title="Help"
+          type="outline"
+        />
+      ),
+  };
   constructor(props) {
-    super(props);   
-   userId = this.props.navigation.getParam('itemId', '');
+    super(props);
+    
+  userId = this.props.navigation.getParam('itemId', '');
    PersonName = this.props.navigation.getParam('name','');
    }
   render() {
+    const nav = this.props.navigation;
     return (
-      <Container>
-          <Header>
-              <Button transparent>
-                  <Icon name="ios-arrow-back" />
-              </Button>
-              <Title>Header</Title>
-              <Button transparent>
-                  <Icon name="ios-menu" />
-              </Button>
-          </Header>
-          <Content>
-              <Text>This is new design</Text>
-          </Content>
-          <Footer>
-              <Button transparent>
-                  <Icon name="ios-call" />
-              </Button>
-
-              <Title>Footer</Title>
-
-              <Button transparent >
-                  <Icon name="ios-chatbubbles" />
-              </Button>
-          </Footer>
-      </Container>
+      <View style={{flex: 0.9}}>
+      
+     
+      <Card containerStyle={{flex:1, justifyContent: "center"}}
+        title='Device List'>
+        <Text style={{marginBottom: 30}}>
+          You can view the list of all available devices.
+        </Text>
+        <Button
+          icon={<Icon name='device-mobile' color='#ffffff' />}
+          backgroundColor='#03A9F4'
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+          onPress={() =>  nav.navigate('DeviceList')}
+          title='VIEW NOW' />
+      </Card>
+       </View>
     );
   }
 }
